bf81e542c17ae61111be9827d0d82746cabefc01
haroon <muhammadharoon9802@gmail.com>
2024-08-08 16:35:51 +0500

    headers removed

--- generateToken.js
+++ generateToken.js
@@ -3,37 +3,40 @@ const AppError = require("./utils/AppError");
 const catchAsync = require("./utils/catchAsync");
 
 let apiToken = null;
+let tokenExpiryTime = null;
+
 const fetchToken = catchAsync(async (req, res, next) => {
     try {
-
-        const response = await axios.post('https://identity.crea.ca/connect/token',
-            {
-                client_id: process.env.CLIENT_USERNAME,
-                client_secret: process.env.CLIENT_PASSWORD,
-                grant_type: "client_credentials",
-                scope: "DDFApi_Read"
-            }, {
+        const response = await axios.post('https://identity.crea.ca/connect/token', {
+            client_id: process.env.CLIENT_USERNAME,
+            client_secret: process.env.CLIENT_PASSWORD,
+            grant_type: "client_credentials",
+            scope: "DDFApi_Read"
+        }, {
             headers: {
                 "Content-Type": "application/x-www-form-urlencoded"
             }
-        }
-        );
+        });
+
         apiToken = response.data.access_token;
-        console.log(apiToken);
+        // Calculate the expiry time as current time plus the token's duration
+        tokenExpiryTime = Date.now() + response.data.expires_in * 1000;
+        console.log(`New token fetched: ${apiToken}`);
     } catch (error) {
-        return new AppError("Failed to refresh token", 500)  //400 for bad request
+        console.error("Failed to refresh token", error);
+        throw new AppError("Failed to refresh token", 500);
     }
-})
+});
 
-
-const getToken = () => {
-    if (!apiToken || Date.now() >= apiToken.expires_in) {
-        // Token is expired or not available, fetch a new one
-        fetchToken();
+const getToken = async () => {
+    if (!apiToken || Date.now() >= tokenExpiryTime) {
+        console.log("Token expired or not available, fetching a new one...");
+        await fetchToken();
     }
     return apiToken;
-}
+};
 
 module.exports = {
-    getToken, fetchToken
-}
+    getToken,
+    fetchToken
+};
